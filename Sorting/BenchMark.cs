1462a90995da77ef64d23125b0e7e22fc4cd6257
Nikolay Okhlopkov <okhlopkov_nm@albank.ru>
2023-02-05 16:17:24 +0900

    Use private variable for benchs

--- Sorting/BenchMark.cs
+++ Sorting/BenchMark.cs
@@ -5,15 +5,15 @@ namespace Sorting;
 [MemoryDiagnoser]
 public class BenchMark
 {
+    private int[] ArrayForSorting { get; set; } = new int[] { 0, 10, 5, 7, 1200, 6, 8, 9 };
     [Benchmark]
     public void BubbleSimpleBench()
     {
-        Bubble.BubbleSimple(new int[] { 0, 10, 5, 7, 1200, 6, 8, 9 });
+        Bubble.BubbleSimple(ArrayForSorting);
     }
-
     [Benchmark]
     public void BubbleWithLimitBench()
     {
-        Bubble.BubbleWithLimit(new int[] { 0, 10, 5, 7, 1200, 6, 8, 9 });
+        Bubble.BubbleWithLimit(ArrayForSorting);
     }
 }
