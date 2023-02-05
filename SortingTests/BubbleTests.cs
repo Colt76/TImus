350ce5bf05684c27202d9476277796a39b0cfdc5
Nikolay Okhlopkov <okhlopkov_nm@albank.ru>
2023-02-05 16:35:26 +0900

    Add tests for bubble approach

---
+++ SortingTests/BubbleTests.cs
@@ -0,0 +1,26 @@
+﻿using Sorting;
+
+namespace SortingTests;
+
+public class BubbleTests
+{
+    private int[] ArrayForSorting { get; set; } = new int[] { 0, 10, 5, 7, 1200, 6, 8, 9 };
+    private int[] ArraySorted { get; set; } = new int[] { 0, 5, 6, 7, 8, 9, 10, 1200 };
+
+    [Fact]
+    public void BubbleSimpleTest()
+    {
+
+        var sorted = Bubble.BubbleSimple(ArrayForSorting);
+
+        Assert.Equal(ArraySorted, sorted);
+    }
+    [Fact]
+    public void BubbleLimitTest()
+    {
+
+        var sorted = Bubble.BubbleWithLimit(ArrayForSorting);
+
+        Assert.Equal(ArraySorted, sorted);
+    }   
+}
