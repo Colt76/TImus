8c1cc6fa81bf66a0e7f2a9d84d03caddfec04546
Nikolay Okhlopkov <okhlopkov_nm@albank.ru>
2023-02-05 14:24:42 +0900

    Create simple bubbles sorting

---
+++ Sorting/Bubble.cs
@@ -0,0 +1,19 @@
+﻿namespace Sorting;
+
+public static partial class Program
+{
+    private static int[] BubbleSimple(int[] array)
+    {
+        for (var i = 0; i < array.Length-1; ++i)
+        {
+            for (var j = 0; j < array.Length - 1; ++j)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    (array[j], array[j + 1]) = (array[j + 1], array[j]); 
+                }
+            }
+        }
+        return array;
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Sorting;
 
-Console.WriteLine("Hello, World!");
+public static partial class Program
+{
+    private static void Main()
+    {
+        var sorting = new int[] { 0, 10, 5, 7, 1200, 6, 8, 9 };
+        var sorted = BubbleSimple(sorting);
+        testing(sorted);
+    }
+
+    private static void testing(int[] sorted)
+    {
+        for (var i = 0; i < sorted.Length; ++i)
+        {
+            Console.WriteLine(sorted[i]);
+            if (i < sorted.Length - 1 && sorted[i] > sorted[i + 1])
+            {
+                Console.WriteLine("Ошибка!!");
+            }
+        }
+    }
+}
